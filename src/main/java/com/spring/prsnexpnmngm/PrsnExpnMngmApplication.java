6efab1aab3dc2037127c1238c5d4e8212b1362ca
Ino <sylee74133@gmail.com>
2024-02-06 17:34:17 +0900

    commit

---
+++ src/main/java/com/spring/prsnexpnmngm/PrsnExpnMngmApplication.java
@@ -0,0 +1,13 @@
+package com.spring.prsnexpnmngm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrsnExpnMngmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PrsnExpnMngmApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/spring/prsnexpnmngm/PrsnExpnMngmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.prsnexpnmngm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrsnExpnMngmApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
